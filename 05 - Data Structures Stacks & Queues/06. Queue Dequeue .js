5171c511bfd2a8a04475fe11ef671de3889d5248
Michael <santana11281@gmail.com>
2021-08-27 02:02:24 -0400

    c

--- 05 - Data Structures Stacks & Queues/06. Queue Dequeue .js
+++ 05 - Data Structures Stacks & Queues/06. Queue Dequeue .js
@@ -21,10 +21,37 @@ class Queue {
         this.length = 1
     }
 
+    Enqueue(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+    
+        this.first = newNode
+        this.last = newNode
+
+
+        }else {
+            this.last.next = newNode
+            this.last = newNode
+
+        }
+        this.length++
+        return this
+    }
+
 
    dequeue(){
 
-    
+    if(this.length === 0) return undefined
+    let temp = this.first
+    if(this.length === 1){
+        this.last = null
+    } else {
+        this.first = this.first.next
+            temp.next = null
+    }
+     this.length--
+        return temp
+
    }
 
 
@@ -37,6 +64,7 @@ let myQueue = new Queue(11)
 
 myQueue.Enqueue(3)
 myQueue.Enqueue(23)
+myQueue.dequeue()
 
 
 console.log(myQueue)
---
+++ 06 - Data Structures Trees/01 - BST Constructor .js
@@ -0,0 +1,22 @@
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+class BST {
+  constructor(value) {
+    this.root = null;
+  }
+}
+
+let myTree = new BST()
+
+
+
+console.log(myTree)
