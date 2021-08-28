b57bd932ce6068c77782adbba1e7ba8be8f11602
Michael <santana11281@gmail.com>
2021-08-27 22:03:17 -0400

    added more

--- 08 - Data Structures Graphs/04 - Graphs Remove Vertex .js
+++ 08 - Data Structures Graphs/04 - Graphs Remove Vertex .js
@@ -23,6 +23,34 @@ addEdge (vertex1,vertex2){
     return false
 }
 
+
+removeEdge(vertex1,vertex2){
+    if(this.adjacencyList[vertex1] && this.adjacencyList[vertex2]){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1]
+        .filter(v => v !== vertex2 )
+
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2]
+        .filter( v => v !== vertex1)
+        return true
+
+    }
+    return false
+
+}
+
+
+removeVertex(vertex){
+
+    if(!this.adjacencyList[vertex]) return undefined
+        while(this.adjacencyList[vertex].length){
+            let temp = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex,temp)
+
+        }
+        delete this.adjacencyList[vertex]
+        return this
+}
+
 }
 
 
@@ -31,7 +59,17 @@ let myGraph = new Graph()
 
 console.log(myGraph.addVertex('A'))
 console.log(myGraph.addVertex('B'))
+console.log(myGraph.addVertex('C'))
+console.log(myGraph.addVertex('D'))
 
 myGraph.addEdge("A","B")
+myGraph.addEdge("A","C")
+myGraph.addEdge("A","D")
+myGraph.addEdge("B","D")
+myGraph.addEdge("C","D")
 
-console.log(myGraph.addEdge("A","B"),"last")
+
+
+
+
+console.log(myGraph.removeVertex("D"))
