92106c3acad19f9157c9b7a467f16e689a03fadf
Michael <santana11281@gmail.com>
2021-08-28 21:39:30 -0400

    new

---
+++ 12 - Algorithms Quick Sort/01 - Pivot .js
@@ -0,0 +1,29 @@
+function swap(array, firstIndex,secondIndex){
+
+    let temp = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = temp
+
+
+}
+
+function pivot(array, pivotIndex=0, endIndex=array.length-1){
+
+    let swapIndex = pivotIndex
+    for(let i = pivotIndex + 1; i <= endIndex ; i++){
+        if(array[i] <array[pivotIndex] ){
+            swapIndex++
+            swap(array,swapIndex,i)
+        }
+
+    }
+    swap(array,pivotIndex,swapIndex)
+
+    return swapIndex
+
+}
+
+let myArr = [4,6,1,7,3,2,5]
+ pivot( myArr)
+
+ console.log(myArr)
